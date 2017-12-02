cc5f4fa505b8eaaf0bbc29af771788d948eb8e56
Konrad Borowski <konrad@borowski.pw>
2017-12-02 08:25:54 +0100

    Day 2 solution

---
+++ day2/src/main.rs
@@ -0,0 +1,53 @@
+#[macro_use]
+extern crate error_chain;
+extern crate itertools;
+
+use itertools::Itertools;
+use std::io::{self, BufRead};
+use std::num::ParseIntError;
+
+error_chain! {
+    errors {
+        EmptyLine {
+            description("empty line retrieved: they have neither minimal nor maximal value")
+        }
+    }
+
+    foreign_links {
+        Io(io::Error);
+        ParseInt(ParseIntError);
+    }
+}
+
+fn checksum<T: BufRead>(input: T) -> Result<u32> {
+    let mut checksum = 0;
+    for line in input.lines() {
+        let (min, max) = line?
+            .split_whitespace()
+            .map(str::parse::<u32>)
+            .minmax_by_key(|result| result.as_ref().ok().cloned())
+            .into_option()
+            .ok_or_else(|| ErrorKind::EmptyLine)?;
+        checksum += max? - min?;
+    }
+    Ok(checksum)
+}
+
+fn run() -> Result<()> {
+    let stdin = io::stdin();
+    println!("{}", checksum(stdin.lock())?);
+    Ok(())
+}
+
+quick_main!(run);
+
+#[cfg(test)]
+mod test {
+    use checksum;
+
+    #[test]
+    fn test_checksum() {
+        let spreadsheet = b"5 1 9 5\n7 5 3\n2 4 6 8\n";
+        assert_eq!(checksum(&spreadsheet[..]).unwrap(), 18);
+    }
+}

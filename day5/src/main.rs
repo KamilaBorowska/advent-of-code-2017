29bfc0f475f0c9ea5ca8d3ed15669e44020e8622
Konrad Borowski <konrad.borowski@kontomatik.com>
2017-12-05 11:19:31 +0100

    Merge 2 day 5 parts

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -11,6 +11,28 @@ error_chain! {
     }
 }
 
+fn steps<F>(jumps: &mut [isize], mut new_pc: F) -> u64
+where
+    F: FnMut(isize) -> isize,
+{
+    let mut pc = 0;
+    let mut steps = 0;
+    while let Some(add_pc) = jumps.get_mut(pc as usize) {
+        pc += *add_pc;
+        *add_pc = new_pc(*add_pc);
+        steps += 1;
+    }
+    steps
+}
+
+fn steps_part1(jumps: &mut [isize]) -> u64 {
+    steps(jumps, |pc| pc + 1)
+}
+
+fn steps_part2(jumps: &mut [isize]) -> u64 {
+    steps(jumps, |pc| if pc >= 3 { pc - 1 } else { pc + 1 })
+}
+
 fn run() -> Result<()> {
     let stdin = io::stdin();
     let mut jumps = stdin
@@ -18,14 +40,8 @@ fn run() -> Result<()> {
         .lines()
         .map(|line| Ok(line?.parse()?))
         .collect::<Result<Vec<isize>>>()?;
-    let mut pc = 0;
-    let mut steps = 0u64;
-    while let Some(add_pc) = jumps.get_mut(pc as usize) {
-        pc += *add_pc;
-        *add_pc += 1;
-        steps += 1;
-    }
-    println!("{}", steps);
+    println!("Part 1: {}", steps_part1(&mut jumps.clone()));
+    println!("Part 2: {}", steps_part2(&mut jumps));
     Ok(())
 }
 
